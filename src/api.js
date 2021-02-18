03da31687acbb01dec01a99d50012fd1147faf81
bruno talarico <bruno.talarico@gmail.com>
2021-02-18 15:44:49 +0000

    api setup
    Signed-off-by: bruno talarico <bruno.talarico@gmail.com>

---
+++ src/api.js
@@ -0,0 +1,16 @@
+//base URL
+
+const base_url = "https://api.rawg.io/api/";
+
+//date
+const currentYear = new Date().getFullYear();
+const currentMonth = String(new Date().getMonth() + 1).padStart(2, "0");
+const currentDay = String(new Date().getDate()).padStart(2, "0");
+const currentDate = `${currentYear}-${currentMonth}-${currentDay}`;
+const lastYear = `${currentYear - 1}-${currentMonth}-${currentDay}`;
+const nextYear = `${currentYear + 1}-${currentMonth}-${currentDay}`;
+
+//popular games
+const popular_games = `games?dates=${lastYear},${currentDate}&ordering=-rating&page_size=20`;
+
+export const popularGamesURL = () => `${base_url}${popular_games}`;
