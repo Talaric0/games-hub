74320ca598e796668b998463bd52bc38a7b1af69
bruno talarico <bruno.talarico@gmail.com>
2021-02-19 10:57:50 +0000

    refactoring, home page added
    Signed-off-by: bruno talarico <bruno.talarico@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
-import React, { useEffect } from "react";
-import { useDispatch } from "react-redux";
-import { loadGames } from "./actions/gamesAction";
+import React from "react";
+//components and pages
+import Home from "./pages/Home";
 
 function App() {
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    dispatch(loadGames());
-  });
-
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <Home />
     </div>
   );
 }
