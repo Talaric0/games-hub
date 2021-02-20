217c8ec71c171487fd177951edfccade0d7b5ff7
bruno talarico <bruno.talarico@gmail.com>
2021-02-20 10:27:45 +0000

    fetch game detail
    Signed-off-by: bruno talarico <bruno.talarico@gmail.com>

--- src/components/Game.js
+++ src/components/Game.js
@@ -2,12 +2,20 @@ import React from "react";
 //Styling and animation
 import styled from "styled-components";
 import { motion } from "framer-motion";
+//redux
+import { useDispatch } from "react-redux";
+import { loadDetail } from "../actions/detailAcion";
 
 export default function Game({ game }) {
-  const { name, released, background_image } = game;
+  const { name, released, background_image, id, short_screenshots } = game;
+
+  const dispatch = useDispatch();
+  const loadDetailHandler = () => {
+    dispatch(loadDetail(id));
+  };
 
   return (
-    <StyledGame>
+    <StyledGame onClick={loadDetailHandler}>
       <h3>{name}</h3>
       <p>{released}</p>
       <img src={background_image} alt={name} />
---
+++ src/components/GameDetail.js
@@ -0,0 +1,18 @@
+import React from "react";
+//Styling and Animation
+import styled from "styled-components";
+import { motion } from "framer-motion";
+//Redux
+import { useSelector } from "react-redux";
+import { useHistory } from "react-router-dom";
+import { smallImage } from "../util";
+//IMAGES
+import playstation from "../img/playstation.svg";
+import steam from "../img/steam.svg";
+import xbox from "../img/xbox.svg";
+import nintendo from "../img/nintendo.svg";
+import apple from "../img/apple.svg";
+import gamepad from "../img/gamepad.svg";
+//Star Images
+import starEmpty from "../img/star-empty.png";
+import starFull from "../img/star-full.png";
